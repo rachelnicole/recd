78ab989c7c56f8d998380c53b70bd3f039d4b9c2
Rachel White <loveless@gmail.com>
2023-01-11 12:45:26 -0500

    ripped out old map library and got markers working

--- components/SimpleMap.js
+++ components/SimpleMap.js
@@ -1,46 +1,55 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import settings from '../settings'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+const onLoad = marker => {
+  console.log('marker: ', marker)
+}
+
 
 class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
-
-  
-
   render() {
     this.props.mapData.map((p, i) => (
       console.log(p)
     ))
 
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100%', width: '50vw' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: settings.googleMaps.apiKey }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          yesIWantToUseGoogleMapApiInternals
+      <LoadScript
+        googleMapsApiKey={settings.googleMaps.apiKey}
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
         >
-          {this.props.mapData.map((p, i) => (
-                <AnyReactComponent
-                key={i}
-                lat={p.latitude}
-                lng={p.longitude}
-                text={p.name}
-              />
-              ))
+          
+          {this.props.mapData.map((p, i) => {
+            const position = {
+              lat: Number(p.latitude),
+              lng: Number(p.longitude)
             }
-        </GoogleMapReact>
-      </div>
-    );
+
+            return (<Marker
+              onLoad={onLoad}
+              position={position }
+              label={p.name}
+            />)
+        })
+            }
+          <></>
+        </GoogleMap>
+      </LoadScript>
+    )
   }
 }
 
